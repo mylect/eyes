2b2eee974162e72b0fd01e1151dd7f1d5d5f7034
mylect <info@mylect.co.jp>
2026-05-10 15:24:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+window.MYLECT_CONFIG = {
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyMzrsxSvQoHKOhAgWseRC7WuuOceFl4RnqFkHTgRIC3SH7r0I0rovuMXobmam3a72h/exec",
+
+  APP_NAME: "Mylect",
+  BRAND_LABEL: "MYLECT CARE CHECK",
+
+  BASIC_PATH: "?mode=basic",
+  CHECK_PATH: "?mode=check"
+};
 
