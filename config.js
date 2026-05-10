0282cda88e5af2d66f446ced5fbb1b376a91226e
mylect <info@mylect.co.jp>
2026-05-10 16:28:17 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 window.MYLECT_CONFIG = {
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyMzrsxSvQoHKOhAgWseRC7WuuOceFl4RnqFkHTgRIC3SH7r0I0rovuMXobmam3a72h/exec",
 
-  APP_NAME: "Mylect",
-  BRAND_LABEL: "MYLECT CARE CHECK",
-
-  BASIC_PATH: "?mode=basic",
-  CHECK_PATH: "?mode=check"
+  LIFF_ID_BASIC: "2010027434-NUnlBPBC",
+  LIFF_ID_CHECK: "2010028367-kJz63s2k"
 };
 
